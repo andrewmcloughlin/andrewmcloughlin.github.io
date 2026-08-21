f894777e9450d59b507a42f342a4af2b9d09db58
Andrew McLoughlin <andrew.mcloughlin@owlstone.co.uk>
2026-08-21 13:36:02 +0100

    fix broken links

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
+const isProduction = process.env.ELEVENTY_ENV === "production";
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/js");
 
     return {
-        pathPrefix: "/portfolio/",
+        pathPrefix: isProduction ? "/portfolio/" : "/",
         dir: {
             input: "src",
             output: "_site"
