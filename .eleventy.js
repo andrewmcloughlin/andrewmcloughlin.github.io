f34fb23a701036145879fa90b692325293832214
Andrew McLoughlin <andrew.mcloughlin@owlstone.co.uk>
2026-08-21 12:51:42 +0100

    Add GitHub Actions deploy workflow and pathPrefix for GitHub Pages

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/js");
 
     return {
+        pathPrefix: "/portfolio/",
         dir: {
             input: "src",
             output: "_site"
