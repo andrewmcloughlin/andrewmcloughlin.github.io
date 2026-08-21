85e91a83309a15fd9f80018b43912f38fadb3d17
Andrew McLoughlin <andrew.mcloughlin@owlstone.co.uk>
2026-08-21 13:49:12 +0100

    rename portfolio link to work

--- src/_data/cv.js
+++ src/_data/cv.js
@@ -1,10 +1,10 @@
 module.exports = {
     qualifications: [
         {
-            degree: "BSc Biomedical Science",
-            institution: "University of Warwick",
-            year: "2011 - 2014",
-            description: "Graduated with 2:1 honours. Thesis: The Metabolism Browser an interactive tool for exploring metabolic pathways built in VBA."
+            degree: "MPhil Cardiovascular Epidemiology",
+            institution: "University of Cambridge",
+            year: "2015 - 2016",
+            description: "Thesis: Investigating the relationship between cardiovascular risk factors and cardiovascular disease in the EPIC-Norfolk cohort."
         },
         {
             degree: "MSc Computational Systems Science",
@@ -13,24 +13,18 @@ module.exports = {
             description: "Graduated with merit. Thesis: Developing signal processing algorithms for actimetry data from a smart watch."
         },
         {
-            degree: "MPhil Cardiovascular Epidemiology",
-            institution: "University of Cambridge",
-            year: "2015 - 2016",
-            description: "Thesis: Investigating the relationship between cardiovascular risk factors and cardiovascular disease in the EPIC-Norfolk cohort."
+            degree: "BSc Biomedical Science",
+            institution: "University of Warwick",
+            year: "2011 - 2014",
+            description: "Graduated with 2:1 honours. Thesis: The Metabolism Browser an interactive tool for exploring metabolic pathways built in VBA."
         }
     ],
     employmentHistory: [
         {
-            role: "Clinical Data Manager",
-            company: "Research in Real Life",
-            duration: "2013 - 2015",
-            description: "Building data collection tools for clinical trials."
-        },
-        {
-            role: "Senior Clinical Data Manager",
-            company: "Research in Real Life",
-            duration: "2015 - 2017",
-            description: "Leading the Clinicial Data Management Team and designing and building data collection tools for clinical trials."
+            role: "Senior Data Engineer & Product Owner",
+            company: "Owlstone Medical",
+            duration: "2020 - Present",
+            description: "Building data pipelines and data infrastructure for clinical trials."
         },
         {
             role: "Senior Clinical Data Manager",
@@ -39,10 +33,16 @@ module.exports = {
             description: "Leading the Clinicial Data Management Team and designing and building data collection tools for clinical trials."
         },
         {
-            role: "Senior Data Engineer & Product Owner",
-            company: "Owlstone Medical",
-            duration: "2020 - Present",
-            description: "Building data pipelines and data infrastructure for clinical trials."
-        }
+            role: "Senior Clinical Data Manager",
+            company: "Research in Real Life",
+            duration: "2015 - 2017",
+            description: "Leading the Clinicial Data Management Team and designing and building data collection tools for clinical trials."
+        },
+        {
+            role: "Clinical Data Manager",
+            company: "Research in Real Life",
+            duration: "2013 - 2015",
+            description: "Building data collection tools for clinical trials."
+        },
     ]
 };
