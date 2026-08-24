2bc7969eff0845954d32c436d4e0103ac4c3b1f2
Andrew McLoughlin <andrew.mcloughlin@owlstone.co.uk>
2026-08-24 10:59:12 +0100

    add mackinak font

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/js");
     eleventyConfig.addPassthroughCopy("src/images");
+    eleventyConfig.addPassthroughCopy("src/fonts");
 
     return {
         pathPrefix: isProduction ? "/portfolio/" : "/",
