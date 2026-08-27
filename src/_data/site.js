6ef2fa412f651e5974fe3d04e8c1c3d0258d801d
Andrew McLoughlin <andrew.mcloughlin@owlstone.co.uk>
2026-08-27 13:55:34 +0100

    add robots and llms.txt

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,4 +1,8 @@
 module.exports = {
     name: "Andrew McLoughlin",
-    description: "A full-stack developer of scientific software."
+    url: "https://andrewmcloughlin.github.io", // adjust if needed based on the previous context
+    description: "A full-stack developer of scientific software.",
+    author: {
+        name: "Andrew McLoughlin"
+    }
 };
