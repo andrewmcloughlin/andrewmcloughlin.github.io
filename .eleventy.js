2dc03ebba2b2f094348715e2eee21412d37d24ed
Andrew McLoughlin <andrew.mcloughlin@owlstone.co.uk>
2026-08-27 13:42:36 +0100

    vendorise fontawesome and fix missing favicon

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/images");
     eleventyConfig.addPassthroughCopy("src/fonts");
     eleventyConfig.addPassthroughCopy("src/files");
+    eleventyConfig.addPassthroughCopy("src/fontawesome");
 
     return {
         pathPrefix: "/",
