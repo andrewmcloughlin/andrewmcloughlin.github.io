79544b33820742c10478fb44e237f2127ed1c8f1
Andrew McLoughlin <andrew.mcloughlin@owlstone.co.uk>
2026-09-02 12:01:04 +0100

    refactor to make more DRY

---
+++ src/_data/gallery.js
@@ -0,0 +1,41 @@
+module.exports = [
+    {
+        title: 'Interactive 3D compound viewer',
+        link: '/portfolio/atlas/',
+        media: '/images/gallery/voc.mp4',
+        type: 'video',
+        videoFormat: 'mp4'
+    },
+    {
+        title: 'OMED Health app screen designs & user flows',
+        link: '/portfolio/omed_app/',
+        media: '/images/gallery/omed.png',
+        type: 'image'
+    },
+    {
+        title: 'VOC Atlas: a public web app for breath science',
+        link: '/portfolio/atlas/',
+        media: '/images/gallery/atlas-web.webm',
+        type: 'video',
+        videoFormat: 'webm'
+    },
+    {
+        title: 'Algorithms to learn actigraphy from actimetry data',
+        link: '/portfolio/tyto/',
+        media: '/images/gallery/actigraphy.png',
+        type: 'image'
+    },
+    {
+        title: 'Interactive mapping of VOC-disease relationships',
+        link: '/portfolio/atlas/',
+        media: '/images/gallery/disease-voc.mp4',
+        type: 'video',
+        videoFormat: 'mp4'
+    },
+    {
+        title: 'Interactive marketing for VOC Atlas',
+        link: '/portfolio/atlas/',
+        media: '/images/gallery/voc-sphere.png',
+        type: 'image'
+    }
+];
