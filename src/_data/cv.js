7a29ac4d070e6ca42eafa672821f3257e47ef89b
Andrew McLoughlin <andrew.mcloughlin@owlstone.co.uk>
2026-09-03 09:53:35 +0100

    add publications section

--- src/_data/cv.js
+++ src/_data/cv.js
@@ -88,5 +88,50 @@ module.exports = {
             duration: "2013 - 2015",
             description: "Building data collection tools for clinical trials."
         },
+    ],
+    publications: [
+        {
+            title: "Age at menarche and risks of all-cause and cardiovascular death: a systematic review and meta-analysis",
+            authors: "Andrew McLoughlin, et al.",
+            year: "2014",
+            journal: "American journal of epidemiology",
+            link: "https://doi.org/10.1093/AJE/KWU113",
+            abstract: "We conducted a systematic review and meta-analysis to investigate the associations between menarcheal age and all-cause and cardiovascular death. Medline, Embase, Scopus, and Web of Knowledge were searched for articles published prior to March 2013 reporting on the associations between menarcheal age and death from all causes or from cardiovascular disease (total cardiovascular disease, ischemic heart disease (IHD), and stroke) in adult women. Nine articles were eligible for inclusion; these reported 5 estimates each for death from all causes and total cardiovascular death, 6 estimates for IHD, and 7 estimates for death from stroke. Our meta-analysis showed that each 1-year increase in age at menarche was associated with a 3% lower relative risk of death from all causes (pooled hazard ratio = 0.97, 95% confidence interval: 0.96, 0.98) with low heterogeneity (I2 = 32.2%). Meta-analysis of 2 cohorts showed a higher risk of death from all causes for women who experienced early menarche (at <12 years of age) versus “not early” menarche (at ≥12 years of age) (pooled hazard ratio = 1.23, 95% confidence interval: 1.10, 1.38; I2 = 0%). An inverse association between age at menarche and death from IHD was observed only among nonsmoking populations or populations with low prevalence of smoking. We found no evidence of association between age at menarche and death from all cardiovascular diseases or stroke. Early menarche was consistently associated with higher risk of death from all causes. Further studies are needed to clarify the role of menarcheal age on cardiovascular outcomes and to investigate the potential modifying role of smoking."
+        },
+        {
+            title: "Determinants of increase FeNO in individuals with suspected asthma",
+            year: "2016",
+            journal: "Journal of Thoracic Disease",
+            link: "https://doi.org/10.21037/JTD.2016.S040",
+            abstract: "Airway inflammation is the fundamental mechanism in asthma and can be measured non-invasively by means of fractional exhaled nitric oxide (FeNO). Both, The National Institute for Health and Care Excellence (NICE) diagnostics guidance 12 and British guideline on the management of asthma, are recommending the inclusion of FeNO testing as part of diagnosis in patients that have an intermediate probability of having asthma. Furthermore, FeNO testing is recommended as an option for monitoring response to corticosteroid treatment in patients with increased FeNO. However, there is a need for establishing routine data predictors for individuals with increased FeNO, to identify those that would benefit from FeNO measurement as a tool for aiding asthma diagnosis and monitoring treatment responsiveness. To provide a real-life predictive model for the identification of individuals at risk of asthma that have increased FeNO."
+        },
+        {
+            title: "Pragmatic trial stepping down Flutiform® in patients maintained on high dose ICS",
+            year: "2016",
+            journal: "Journal of Thoracic Disease",
+            link: "https://doi.org/10.21037/JTD.2016.S039",
+            abstract: "Global Initiative for Asthma (GINA) guidelines recommend a gradual step-down of asthma therapy if asthma has been well-controlled for at least 3 months. In real life, however, many well-controlled patients are prescribed high doses of inhaled corticosteroids (ICS), and are therefore unnecessarily at risk of systemic side effects associated with long-term ICS use. To encourage and facilitate step-down in a clinical setting, further pragmatic studies are required to demonstrate that good control of asthma can be maintained with a lower dose of ICS in stable and controlled patients, and to explore potential predictors for response to step-down.To test if good control of asthma can be maintained in adult patients previously stable on Flutiform® 250 (250 mcg fluticasone/10 mcg formoterol) for 12 weeks, after step-down to Flutiform® 125 (250 mcg fluticasone/5 mcg formoterol)."
+        },
+        {
+            title: "Pragmatic trial comparing continuing Seretide® MDI with changing to Flutiform® in asthma",
+            year: "2016",
+            journal: "Journal of Thoracic Disease",
+            link: "https://doi.org/10.21037/JTD.2016.S038",
+            abstract: "Selecting the most appropriate asthma therapy for individual patients can be challenging owing to limited data on real-life effectiveness of inhaled corticosteroid (ICS)/long-acting beta2-agonist (LABA) combinations available. Various factors, such as ease of use of the device and speed of onset of the LABA, can influence inhaler technique and adherence, thereby affecting clinical outcomes in real-life patients. Further studies are needed to evaluate whether efficacy of new asthma therapies demonstrated in clinical trials translate to effectiveness in a pragmatic setting.To compare the real-life effectiveness of switching to Flutiform® 250 (250 mcg fluticasone/10 mcg formoterol) from Seretide® 250 Evohaler® (250 mcg fluticasone/25 mcg salmeterol) in adult patients with asthma."
+        },
+        {
+            title: "A Randomized Pragmatic Trial of Changing to and Stepping Down Fluticasone/Formoterol in Asthma",
+            year: "2017",
+            journal: "The Journal of Allergy and Clinical Immunology: In Practice",
+            link: "https://www.sciencedirect.com/science/article/abs/pii/S2213219817300934",
+            abstract: "In a randomized controlled, pragmatic, open-label trial, 225 well-controlled patients with asthma were randomized (1:2) to maintain high-dose fluticasone propionate/salmeterol xinafoate (FP/SAL, 1000/100 μg) or switch to FP/FOR (1000/40 μg) daily for 12 weeks (phase 1). One hundred sixteen patients stable on FP/FOR at week 12 were subsequently randomized (1:1) to maintain this therapy, or stepped down to FP/FOR (500/20 μg) daily for 12 weeks (phase 2). The primary end point was the 7-question Asthma Control Questionnaire (ACQ7) score."
+        },
+        {
+            title: "Meeting abstracts from the Respiratory Effectiveness Group 2015 Winter Summit &ndash; databases and registries around the world: maximizing the yield",
+            year: "2015",
+            journal: "Pragmatic and Observational Research",
+            link: "https://doi.org/10.2147/POR.S84517",
+            abstract: ""
+        }
     ]
 };
