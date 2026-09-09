ca8f39beadac077124717988fc007088143e79c2
Andrew McLoughlin <andrew.mcloughlin@owlstone.co.uk>
2026-09-09 10:54:09 +0100

    style and publication improvements

--- src/_data/cv.js
+++ src/_data/cv.js
@@ -91,12 +91,97 @@ module.exports = {
     ],
     publications: [
         {
+            title: "A breathomics database to identify, contextualise and validate biomarkers of respiratory conditions",
+            year: "2024",
+            abstract: "Breath-based biomarker development relies on advanced technology to collect and analyse volatile organic compounds (VOCs). Traditionally, untargetted GC-MS analysis is prefered for biomarker discover…",
+            journal: "European Respiratory Journal",
+            link: "https://publications.ersnet.org/content/erj/64/suppl68/pa1710"
+        },
+        {
+            title: "High-quality identification of volatile organic compounds (VOCs) originating from breath",
+            year: "2024",
+            abstract: "Objectives: To build a reliable breath collection and analysis method that can produce a comprehensive list of known VOCs in the breath of a heterogeneous human population.",
+            journal: "Springer Nature",
+            link: "https://link.springer.com/article/10.1007/s11306-024-02163-6",
+        },
+        {
+            title: "A Randomized Pragmatic Trial of Changing to and Stepping Down Fluticasone/Formoterol in Asthma",
+            year: "2017",
+            journal: "The Journal of Allergy and Clinical Immunology: In Practice",
+            link: "https://www.sciencedirect.com/science/article/abs/pii/S2213219817300934",
+            abstract: "In a randomized controlled, pragmatic, open-label trial, 225 well-controlled patients with asthma were randomized (1:2) to maintain high-dose fluticasone propionate/salmeterol xinafoate (FP/SAL, 1000/100 μg) or switch to FP/FOR (1000/40 μg) daily for 12 weeks (phase 1). One hundred sixteen patients stable on FP/FOR at week 12 were subsequently randomized (1:1) to maintain this therapy, or stepped down to FP/FOR (500/20 μg) daily for 12 weeks (phase 2). The primary end point was the 7-question Asthma Control Questionnaire (ACQ7) score."
+        },
+        {
             title: "Age at menarche and risks of all-cause and cardiovascular death: a systematic review and meta-analysis",
             authors: "Andrew McLoughlin, et al.",
             year: "2014",
             journal: "American journal of epidemiology",
             link: "https://doi.org/10.1093/AJE/KWU113",
             abstract: "We conducted a systematic review and meta-analysis to investigate the associations between menarcheal age and all-cause and cardiovascular death. Medline, Embase, Scopus, and Web of Knowledge were searched for articles published prior to March 2013 reporting on the associations between menarcheal age and death from all causes or from cardiovascular disease (total cardiovascular disease, ischemic heart disease (IHD), and stroke) in adult women. Nine articles were eligible for inclusion; these reported 5 estimates each for death from all causes and total cardiovascular death, 6 estimates for IHD, and 7 estimates for death from stroke. Our meta-analysis showed that each 1-year increase in age at menarche was associated with a 3% lower relative risk of death from all causes (pooled hazard ratio = 0.97, 95% confidence interval: 0.96, 0.98) with low heterogeneity (I2 = 32.2%). Meta-analysis of 2 cohorts showed a higher risk of death from all causes for women who experienced early menarche (at <12 years of age) versus “not early” menarche (at ≥12 years of age) (pooled hazard ratio = 1.23, 95% confidence interval: 1.10, 1.38; I2 = 0%). An inverse association between age at menarche and death from IHD was observed only among nonsmoking populations or populations with low prevalence of smoking. We found no evidence of association between age at menarche and death from all cardiovascular diseases or stroke. Early menarche was consistently associated with higher risk of death from all causes. Further studies are needed to clarify the role of menarcheal age on cardiovascular outcomes and to investigate the potential modifying role of smoking."
+        },
+        {
+            title: "Introducing the Breath Biopsy® VOC Atlas: A List of Microbiome-Associated VOCs for Breath-Based Biomarker Discovery",
+            year: "2024",
+            abstract: "Volatile organic compound (VOC) biomarkers in exhaled breath offer a promising solution to safe, non-invasive diagnostics in multiple disease settings. Some of these VOCs are linked to the gastrointe…",
+            journal: "AGC",
+            link: "https://www.owlstonemedical.com/wp-content/uploads/2024/09/2024-08-ACG-Poster-Atlas-Final-Copy.pdf",
+        },
+        {
+            title: "Breath Biopsy® Technology",
+            year: "2024",
+            abstract: "Many breath-based studies have been performed in several disease conditions, but there are few validated breath-based biomarkers currently in clinical use. The complex and unknown composition of the …",
+            journal: "Poster for Digestive Disease Week",
+            link: "https://www.owlstonemedical.com/wp-content/uploads/2024/06/2024-03-DDW-Poster.pdf",
+        },
+        {
+            title: "Introducing the VOC Atlas: A List of Microbiome-Associated VOCs for Breath-Based Biomarker Discovery",
+            year: "2024",
+            abstract: "The gastrointestinal (GI) microbiome plays a crucial role in digestive health. Apart from providing colonization resistance to pathogens, it produces a myriad of metabolites that function to enforce …",
+            journal: "The American Journal of Gastroenterology",
+            link: "https://journals.lww.com/ajg/abstract/10.14309/01.ajg.0001030864.79280.76~s374introducing-the-voc-atlas-a-list-of?redirectionsource=fulltextview"
+        },
+        {
+            title: "Analyzing Metabolic Responses to a Standardized Meal Challenge Using Exhaled Breath",
+            year: "2024",
+            abstract: "Conclusion: This study underscores the utility of breath analysis in advancing our understanding of metabolic responses to dietary stimuli. The platform effectively identified established nutrition a…",
+            journal: "The American Journal of Gastroenterology",
+            link: "https://journals.lww.com/ajg/abstract/10.14309/01.ajg.0001030860.11676.7b~s373analyzing-metabolic-responses-to-a-standardized-meal?redirectionsource=fulltextview"
+        },
+        {
+            title: "Optimized tools for the reliable sampling and analysis of breath for research and clinical use",
+            year: "2024",
+            abstract: "Analysis of biological samples such as blood, urine, and fecal matter is essential in both clinical research and clinical practice. Informative compounds within these sampling mediums are quantified …",
+            journal: "White Paper",
+            link: "https://www.owlstonemedical.com/media/uploads/files/Breath_Biopsy_Whitepaper.pdf"
+        },
+        {
+            title: "Introducing Breath Biopsy® VOC Atlas: A List of Known Breath VOCS for Breath-based Biomarker Discovery",
+            year: "2024",
+            abstract: "The Atlas study was designed to provide a comprehensive catalogue of VOCs identified on exhaled human breath, the Breath Biopsy VOC Atlas, to support development of breath VOC biomarkers and diagnost…",
+            journal: "Poster at the Breath Biopsy Conference",
+            link: "https://www.owlstonemedical.com/media/uploads/files/2023-02_VOC_Atlas_Poster_1.pdf"
+        },
+
+        {
+            title: "Optimized breath analysis: customized analytical methods and enhanced workflow for broader detection of VOCs",
+            year: "2024",
+            abstract: "Breath Volatile organic compounds (VOCs) are promising biomarkers for clinical purposes due to their unique properties. Translation of VOC biomarkers into the clinic depends on identification and val…",
+            journal: "Springer Nature",
+            link: "https://link.springer.com/article/10.1007/s11306-024-02218-8"
+        },
+        {
+            title: "Progress and challenges of developing volatile metabolites from exhaled breath as a biomarker platform",
+            year: "2024",
+            abstract: "The multitude of metabolites generated by physiological processes in the body can serve as valuable biomarkers for many clinical purposes. They can provide a window into relevant metabolic pathways f…",
+            journal: "Springer Nature",
+            link: "https://link.springer.com/article/10.1007/s11306-024-02142-x"
+        },
+        {
+            title: "Establishing breath as a biomarker platform—take home messages from the Breath Biopsy Conference 2023",
+            year: "2023",
+            abstract: "The annual Breath Biopsy Conference hosted by Owlstone Medical gathers together the leading experts, early career researchers, and physicians working with breath as a biomarker platform for clinical …",
+            journal: "Editorial for the Breat Biopsy Conference",
+            link: "https://link.springer.com/article/10.1007/s11306-024-02142-x"
         },
         {
             title: "Determinants of increase FeNO in individuals with suspected asthma",
@@ -120,14 +205,7 @@ module.exports = {
             abstract: "Selecting the most appropriate asthma therapy for individual patients can be challenging owing to limited data on real-life effectiveness of inhaled corticosteroid (ICS)/long-acting beta2-agonist (LABA) combinations available. Various factors, such as ease of use of the device and speed of onset of the LABA, can influence inhaler technique and adherence, thereby affecting clinical outcomes in real-life patients. Further studies are needed to evaluate whether efficacy of new asthma therapies demonstrated in clinical trials translate to effectiveness in a pragmatic setting.To compare the real-life effectiveness of switching to Flutiform® 250 (250 mcg fluticasone/10 mcg formoterol) from Seretide® 250 Evohaler® (250 mcg fluticasone/25 mcg salmeterol) in adult patients with asthma."
         },
         {
-            title: "A Randomized Pragmatic Trial of Changing to and Stepping Down Fluticasone/Formoterol in Asthma",
-            year: "2017",
-            journal: "The Journal of Allergy and Clinical Immunology: In Practice",
-            link: "https://www.sciencedirect.com/science/article/abs/pii/S2213219817300934",
-            abstract: "In a randomized controlled, pragmatic, open-label trial, 225 well-controlled patients with asthma were randomized (1:2) to maintain high-dose fluticasone propionate/salmeterol xinafoate (FP/SAL, 1000/100 μg) or switch to FP/FOR (1000/40 μg) daily for 12 weeks (phase 1). One hundred sixteen patients stable on FP/FOR at week 12 were subsequently randomized (1:1) to maintain this therapy, or stepped down to FP/FOR (500/20 μg) daily for 12 weeks (phase 2). The primary end point was the 7-question Asthma Control Questionnaire (ACQ7) score."
-        },
-        {
-            title: "Meeting abstracts from the Respiratory Effectiveness Group 2015 Winter Summit &ndash; databases and registries around the world: maximizing the yield",
+            title: "Meeting abstracts from the Respiratory Effectiveness Group 2015 Winter Summit; databases and registries around the world: maximizing the yield",
             year: "2015",
             journal: "Pragmatic and Observational Research",
             link: "https://doi.org/10.2147/POR.S84517",
