4ddddb09380fc85ff7bd369b72d266958b388167
Andrew McLoughlin <andrew.mcloughlin@owlstone.co.uk>
2026-09-23 10:37:57 +0100

    add image transformation pipeline

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,38 @@
+const Image = require("@11ty/eleventy-img");
+const path = require("path");
+
+async function imageShortcode(src, alt, cls = "") {
+    if (alt === undefined) {
+        alt = "";
+    }
+
+    let imgPath = src;
+    if (src.startsWith('/')) {
+        imgPath = path.join('./src', src);
+    } else if (!src.startsWith('./src/')) {
+        imgPath = path.join('./src/images', src);
+    }
+
+    let metadata = await Image(imgPath, {
+        widths: [400, 800, "auto"],
+        formats: ["avif", "webp", "auto"],
+        urlPath: "/images/",
+        outputDir: "./_site/images/"
+    });
+
+    let imageAttributes = {
+        alt,
+        class: cls,
+        loading: "lazy",
+        decoding: "async",
+    };
+
+    return Image.generateHTML(metadata, imageAttributes);
+}
+
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+
     eleventyConfig.addCollection("stackItems", function (collectionApi) {
         const items = collectionApi.getFilteredByTag("portfolio");
         const seen = new Set();
